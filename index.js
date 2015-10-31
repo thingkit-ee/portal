973536bfbaa24ae803189f85f30ff9c596facdaa
bogdan <bogdan@transferwise.com>
2015-10-31 10:15:13 +0200

    ololo

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var Firebase = require("firebase");
 var express = require('express');
 var app = express();
 
@@ -16,5 +17,40 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+var rootFirebase = new Firebase("https://thingkit.firebaseio.com/app/data");
+rootFirebase.authWithCustomToken('1U46cpva90E1oa8rqdiQMEnqg89Vefjc7NW7QIph', function(error, authData) {
+    if (error) {
+        console.log("Login Failed!", error);
+    } else {
+        console.log("Login Succeeded!", authData);
+    }
+});
+
+app.get('/data', function(request, response) {
+    var usersRef = rootFirebase.child('users');
+    usersRef.update({
+        admin: {
+            login: "admin123",
+            password: "q1w2e3r4"
+        },
+        user: {
+            login: "user",
+            password: "q1w2e3r4"
+        }
+    });
+    response.send('Done!')
+});
+
+app.get('/login', function(request, response) {
+  var FirebaseTokenGenerator = require("firebase-token-generator");
+  var tokenGenerator = new FirebaseTokenGenerator("pvFPrznHD8GecmraJbDvQ7wwVaklEafFBxPkQFxs");
+  var token = tokenGenerator.createToken(
+      {uid: "1", some: "arbitrary", data: "here"},
+      {admin: true}
+  );
+
+  response.send('Token' + token)
+})
 
 
